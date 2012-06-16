c61ba084470a346f6ee21e88081135209244800a
Timo H <timo@dealmachine.net>
2012-06-16 17:31:28 +0300

    Transaction validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,9 @@ class Transaction
   include Mongoid::Document
   field :date, :type => Date
   field :amount, :type => Float
+  
+  attr_accessible :amount, :date
+  validates_numericality_of :amount
+  validates_presence_of :amount, :date 
+  
 end
