cdaada2cb33b7ccbb2513aea12b362ad21ee863b
Timo H <timo@dealmachine.net>
2012-06-16 15:14:24 +0300

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FourCash::Application.config.secret_token = 'fb04f459bd17ba1a650a3995be3e159f64c70d21d872e55f71d72b405e612870ab743c0f47fb8932c870ffd49fde005aa5b5e9d15eae863d1392e6f78d5221ab'
